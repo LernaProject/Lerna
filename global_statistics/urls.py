4b123a4ac18ec1540ab47a467a49ee7b3e2ebf2b
SoVictor <SoVictor_tomsk@hotmail.com>
2016-11-13 01:20:05 +0700

    Add best time statistic page
    По ссылке "Статистика" на странице любой задачи можно перейти к рейтингу
    использованного решениями на неё времени.

--- global_statistics/urls.py
+++ global_statistics/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 
-from .views import RatingIndexView, AttemptsView
+from .views import RatingIndexView, AttemptsView, BestTimeView
 
 urlpatterns = (
     url(r'^rating$', RatingIndexView.as_view(), name='rating'),
     url(r'^rating/(?P<page>\d+)$', RatingIndexView.as_view(), name='rating'),
     url(r'^attempts$', AttemptsView.as_view(), name='attempts'),
     url(r'^attempts/(?P<page>\d+)$', AttemptsView.as_view(), name='attempts'),
+    url(r'^best_time/(?P<problem_id>\d+)$', BestTimeView.as_view(), name='best_time'),
+    url(r'^best_time/(?P<problem_id>\d+)/(?P<page>\d+)$', BestTimeView.as_view(), name='best_time'),
 )
--- global_statistics/views.py
+++ global_statistics/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView
-from django.db.models import Count, Q
+from django.db.models import Count, Q, Min
 
 from users.models import User
 from core.models import Attempt, Problem
+
 
 class RatingIndexView(ListView):
     template_name = 'global_statistics/rating.html'
@@ -63,3 +64,47 @@ class AttemptsView(ListView):
                 .order_by('-created_at')
             )
         return attempts
+
+
+class BestTimeView(ListView):
+    template_name = 'global_statistics/best_time.html'
+    context_object_name = 'user_list'
+    allow_empty = True
+    paginate_by = 25
+    paginate_orphans = 1
+
+    def get_queryset(self):
+        problem = Problem.objects.get(id=self.kwargs['problem_id'])
+        users = (
+            User
+            .objects
+            .filter(
+                Q(attempt__problem_in_contest__contest__is_admin=False),
+                Q(attempt__problem_in_contest__problem_id=problem.id),
+                Q(attempt__result='Accepted') | Q(attempt__result='Tested', attempt__score__gt=99.99),
+            )
+            .annotate(best_time=Min('attempt__used_time'))
+            .annotate(compiler=Min('attempt__compiler__name'))
+            .order_by('best_time')
+        )
+
+        if len(users) > 0:
+            rank_top = 0
+            rank_bottom = 0
+            for i in range(1, len(users) + 1):
+                rank_bottom += 1
+                if i == len(users) or users[i].best_time != users[i - 1].best_time:
+                    if rank_top == rank_bottom - 1:
+                        users[rank_top].rank = '{0}'.format(rank_top + 1)
+                    else:
+                        for rank in range(rank_top, rank_bottom):
+                            users[rank].rank = '{0}-{1}'.format(rank_top + 1, rank_bottom)
+                    rank_top = rank_bottom
+
+        return users
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        problem = Problem.objects.get(id=self.kwargs['problem_id'])
+        context.update(problem=problem)
+        return context
