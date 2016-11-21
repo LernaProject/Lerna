fb0b9a103d72919d030aa2c56c57452dddea6d46
SoVictor <SoVictor_tomsk@hotmail.com>
2016-11-22 00:14:24 +0700

    Add a page with links to all trainings. where some problem can be found
    На страницу с условием задачи добавилась ссылка "найти в тренировках",
    ведущая на страницу со всеми тренировками, в которых задача может быть
    сдана

--- global_statistics/urls.py
+++ global_statistics/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from .views import RatingIndexView, AttemptsView, BestTimeView
+from .views import RatingIndexView, AttemptsView, BestTimeView, ProblemInTrainingsView
 
 urlpatterns = (
     url(r'^rating$', RatingIndexView.as_view(), name='rating'),
@@ -9,4 +9,5 @@ urlpatterns = (
     url(r'^attempts/(?P<page>\d+)$', AttemptsView.as_view(), name='attempts'),
     url(r'^best_time/(?P<problem_id>\d+)$', BestTimeView.as_view(), name='best_time'),
     url(r'^best_time/(?P<problem_id>\d+)/(?P<page>\d+)$', BestTimeView.as_view(), name='best_time'),
+    url(r'^problem_in_trainings/(?P<problem_id>\d+)$', ProblemInTrainingsView.as_view(), name='problem_in_trainings'),
 )
--- global_statistics/views.py
+++ global_statistics/views.py
@@ -1,8 +1,7 @@
 from django.views.generic import ListView
-from django.db.models import Count, Q, Min
-
+from django.db.models import Count, F, Q, Min
 from users.models import User, rank_users
-from core.models import Attempt, Problem
+from core.models import Attempt, Problem, Contest
 
 
 class RatingIndexView(ListView):
@@ -18,7 +17,7 @@ class RatingIndexView(ListView):
             .objects
             .filter(
                 Q(attempt__problem_in_contest__contest__is_admin=False),
-                Q(attempt__result='Accepted') | Q(attempt__result='Tested', attempt__score__gt=99.99),
+                Q(attempt__result='Accepted') | Q(attempt__result='Tested', attempt__score__gt=99.99)
             )
             .annotate(problems_solved=Count('attempt__problem_in_contest__problem', distinct=True))
             .order_by('-problems_solved')
@@ -68,7 +67,7 @@ class BestTimeView(ListView):
             .filter(
                 Q(attempt__problem_in_contest__contest__is_admin=False),
                 Q(attempt__problem_in_contest__problem_id=problem.id),
-                Q(attempt__result='Accepted') | Q(attempt__result='Tested', attempt__score__gt=99.99),
+                Q(attempt__result='Accepted') | Q(attempt__result='Tested', attempt__score__gt=99.99)
             )
             .annotate(best_time=Min('attempt__used_time'))
             .annotate(compiler=Min('attempt__compiler__name'))
@@ -76,6 +75,33 @@ class BestTimeView(ListView):
         )
         rank_users(users, 'best_time')
         return users
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        problem = Problem.objects.get(id=self.kwargs['problem_id'])
+        context.update(problem=problem)
+        return context
+
+
+class ProblemInTrainingsView(ListView):
+    template_name = 'global_statistics/problem_in_trainings.html'
+    context_object_name = 'training_list'
+
+    def get_queryset(self):
+        problem = Problem.objects.get(id=self.kwargs['problem_id'])
+        trainings = (
+            Contest
+            .objects
+            .filter(
+                problem_in_contest_set__problem_id=problem.id,
+                is_training=True,
+                is_admin=False,
+            )
+            .only('id', 'name')
+            .annotate(problem_number=F('problem_in_contest_set__number'))
+            .order_by('name')
+        )
+        return trainings
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
