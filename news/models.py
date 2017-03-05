6709d10207517b275d3dd1dd19b3c9abb266c514
Viers <krusnik.viers@gmail.com>
2017-03-06 00:48:33 +0300

    Display news content on index page

--- news/models.py
+++ news/models.py
@@ -1,6 +1,7 @@
 from django.db    import models
 from users.models import User
 
+
 class News(models.Model):
     title       = models.CharField(max_length=255)
     description = models.TextField()
@@ -8,6 +9,10 @@ class News(models.Model):
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
     visible     = models.BooleanField(default=True)
+
+    @property
+    def short_description(self):
+        return self.description.split('<more>')[0]
 
     class Meta:
         db_table            = 'news'
