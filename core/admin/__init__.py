8f9f217b7f2274ae9bd69b2a9699f8b0a9f099a8
Nickolay Bukreyev <buknik95@yandex.ru>
2017-04-09 06:36:27 +0700

    Hide `messages_extends` from the admin for now

--- core/admin/__init__.py
+++ core/admin/__init__.py
@@ -1,6 +1,14 @@
+from django.contrib import admin
+
+import messages_extends.admin  # Ensure it got registered.
+import messages_extends.models
+
 from .attempt       import *
 from .clarification import *
 from .compiler      import *
 from .contest       import *
 from .notification  import *
 from .problem       import *
+
+
+admin.site.unregister(messages_extends.models.Message)
