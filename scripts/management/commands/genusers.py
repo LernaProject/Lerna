7d544e4fb6034c464b25ddab1523c7708bceb638
Nickolay Bukreyev <buknik95@yandex.ru>
2017-05-12 15:22:11 +0700

    Modify `genusers` script so that it takes a `listfile` argument

--- scripts/management/commands/genusers.py
+++ scripts/management/commands/genusers.py
@@ -1,3 +1,5 @@
+import argparse
+
 from django.core import management, exceptions
 from django.db   import transaction
 
@@ -5,19 +7,34 @@ from core.models  import Contest, UserInContest
 from users.models import User
 
 
-def generate_users(mask, indices):
-    for index in indices:
+def check_mask(mask):
+    try:
+        mask % 0
+    except TypeError:
+        raise argparse.ArgumentTypeError("must contain exactly one '%' placeholder")
+    except ValueError as e:
+        raise argparse.ArgumentTypeError(str(e))
+    else:
+        return mask
+
+
+def generate_users(mask, info):
+    for index, username in info:
         login = mask % index
         password = User.objects.make_random_password()
-        user = User(login=login, username=login, rights=0x0)
+        user = User(login=login, username=username, rights=0x0)
         user.set_password(password)
         yield user, password
 
 
 class Command(management.base.BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('name_mask', help='User login/name mask (with, e.g., %%02d).')
-        parser.add_argument('count', type=int)
+        parser.add_argument('listfile', type=argparse.FileType(encoding='utf-8-sig'), help="""
+            Text file containing user names, each on a single line.
+        """)
+        parser.add_argument('login_mask', type=check_mask, help="""
+            User login mask (with, e.g., %%02d).
+        """)
         parser.add_argument('--start', type=int, default=0, help="""
             Number for the first user (default: %(default)s).
         """)
@@ -25,16 +42,11 @@ class Command(management.base.BaseCommand):
             Contest ID to register the users for. Can be given multiple times.
         """)
 
-    def handle(self, name_mask, count, *, start=0, contest=None, **options):
+    def handle(self, listfile, login_mask, *, start=0, contest=None, **options):
         contest_ids = contest or [ ]
-        try:
-            name_mask % 0
-        except TypeError:
-            raise management.CommandError("name_mask must contain exactly one '%' placeholder")
-        except ValueError as e:
-            raise management.CommandError(str(e))
-
-        pairs = list(generate_users(name_mask, range(start, start + count)))
+        usernames = [line[:-1] for line in listfile if len(line) > 1]
+        listfile.close()
+        pairs = list(generate_users(login_mask, enumerate(usernames, start)))
         contests = Contest.objects.in_bulk(contest_ids)
         if len(contests) != len(contest_ids):
             raise management.CommandError('Contests: only %s found' % (contests, ))
@@ -48,4 +60,4 @@ class Command(management.base.BaseCommand):
             raise management.CommandError(str(e))
 
         for user, password in pairs:
-            print(user.login, password)
+            print(user.username, user.login, password, sep='\t')
