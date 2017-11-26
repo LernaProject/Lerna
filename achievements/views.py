ba5469a96a270f652fb0bf5a482b7e2dd562f637
sovictor <sovictor_tomsk@hotmail.com>
2017-11-26 17:10:04 +0700

    Styled achievements

--- achievements/views.py
+++ achievements/views.py
@@ -1,7 +1,7 @@
-from django.views.generic       import TemplateView
+from django.views.generic import TemplateView
 
-from achievements.models import Achievement
-from users.models        import User
+from achievements.models  import Achievement
+from users.models         import User
 
 class AchievementsView(TemplateView):
     template_name = 'achievements/achievements.html'
@@ -21,6 +21,9 @@ class AchievementsView(TemplateView):
                 unlocked.append(status)
             else:
                 locked.append(status)
+
+        unlocked = sorted(unlocked, key=lambda x: x.earned_at, reverse=True)
+        locked = sorted(locked, key=lambda x: (x.progress_percent, -x.achievement.points), reverse=True)
 
         context.update(
             user=user,
